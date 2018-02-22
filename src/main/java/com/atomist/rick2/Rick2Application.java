f7e9042e266509c46510c71bdc5f860c27b630e4
Rod Johnson <johnsonroda@gmail.com>
2018-02-22 13:41:34 -0800

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/rick2/Rick2Application.java
@@ -0,0 +1,12 @@
+package com.atomist.rick2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rick2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rick2Application.class, args);
+	}
+}
